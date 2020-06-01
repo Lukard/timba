38b383945d5d12a8a5338e15d3645a3ab111ff05
Lukard <Lukard@users.noreply.github.com>
2020-06-01 05:01:37 +0000

    deploy: 22c0696c8ed6b31d6a487242b5188d8a53fb4206

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,7 +1,7 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "4dbf1585f5d5f0f9d1f1b63739618814",
+  "main.dart.js": "59e4dd07412cc5d635b05786492004e2",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/LICENSE": "8c61598cf05798fd3630d4a2c1131d4e",
