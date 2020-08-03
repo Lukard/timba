7b7fd9134c9c67d706c15cc900b95a79fa928708
Ruben Abad <ruben.abadsanchez@xing.com>
2020-08-03 16:28:38 +0200

    Add privacy policy test

---
+++ test/privacy_policy_test.dart
@@ -0,0 +1,12 @@
+import 'package:Timba/privacy_policy.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Privacy Policy is shown', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: PrivacyPolicy()));
+
+    expect(find.text('No personal information is gathered. Enjoy! :)'),
+        findsOneWidget);
+  });
+}
